f819dc0dbc3305fdf72be50023cbc8ef9c0acb07
iqbaldiit <iqbaldiit@gmail.com>
2025-01-07 14:26:41 +0600

    N/A

---
+++ SQL 50/Recyclable and Low Fat Products.sql
@@ -0,0 +1,61 @@
+/*
+Table: Products
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| product_id  | int     |
+| low_fats    | enum    |
+| recyclable  | enum    |
++-------------+---------+
+product_id is the primary key (column with unique values) for this table.
+low_fats is an ENUM (category) of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
+recyclable is an ENUM (category) of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
+ 
+
+Write a solution to find the ids of products that are both low fat and recyclable.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Products table:
++-------------+----------+------------+
+| product_id  | low_fats | recyclable |
++-------------+----------+------------+
+| 0           | Y        | N          |
+| 1           | Y        | Y          |
+| 2           | N        | Y          |
+| 3           | Y        | Y          |
+| 4           | N        | N          |
++-------------+----------+------------+
+Output: 
++-------------+
+| product_id  |
++-------------+
+| 1           |
+| 3           |
++-------------+
+Explanation: Only products 1 and 3 are both low fat and recyclable.
+*/
+
+--- Create Table 
+CREATE TABLE Products (product_id INT, low_fats CHAR(1), recyclable CHAR(1))
+
+--- Insert Data
+INSERT INTO Products VALUES (0,'Y','N')
+INSERT INTO Products VALUES (1,'Y','Y')
+INSERT INTO Products VALUES (2,'N','Y')
+INSERT INTO Products VALUES (3,'Y','Y')
+INSERT INTO Products VALUES (4,'N','N')
+
+--- Solution
+SELECT product_id FROM Products WHERE low_fats='Y' AND recyclable='Y'
+--- Drop table
+
+DROP TABLE Products
