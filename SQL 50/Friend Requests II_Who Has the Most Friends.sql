f29aec8a3c5da6e10e04d5d00d16ed5bf16d2850
iqbaldiit <iqbaldiit@gmail.com>
2025-01-10 11:14:38 +0600

    SQL

---
+++ SQL 50/Friend Requests II_Who Has the Most Friends.sql
@@ -0,0 +1,62 @@
+--Table: RequestAccepted
+
+--+----------------+---------+
+--| Column Name    | Type    |
+--+----------------+---------+
+--| requester_id   | int     |
+--| accepter_id    | int     |
+--| accept_date    | date    |
+--+----------------+---------+
+--(requester_id, accepter_id) is the primary key (combination of columns with unique values) for this table.
+--This table contains the ID of the user who sent the request, the ID of the user who received the request, and the date when the request was accepted.
+ 
+
+--Write a solution to find the people who have the most friends and the most friends number.
+
+--The test cases are generated so that only one person has the most friends.
+
+--The result format is in the following example.
+
+ 
+
+--Example 1:
+
+--Input: 
+--RequestAccepted table:
+--+--------------+-------------+-------------+
+--| requester_id | accepter_id | accept_date |
+--+--------------+-------------+-------------+
+--| 1            | 2           | 2016/06/03  |
+--| 1            | 3           | 2016/06/08  |
+--| 2            | 3           | 2016/06/08  |
+--| 3            | 4           | 2016/06/09  |
+--+--------------+-------------+-------------+
+--Output: 
+--+----+-----+
+--| id | num |
+--+----+-----+
+--| 3  | 3   |
+--+----+-----+
+--Explanation: 
+--The person with id 3 is a friend of people 1, 2, and 4, so he has three friends in total, which is the most number than any others.
+ 
+
+--Follow up: In the real world, multiple people could have the same most number of friends. Could you find all these people in this case?
+
+--create table
+CREATE TABLE RequestAccepted (requester_id int ,accepter_id  int ,accept_date  date)
+
+--insert data
+INSERT INTO RequestAccepted VALUES (1,2,'2016/06/03'),(1,3,'2016/06/08'),(2,3,'2016/06/08'),(3,4,'2016/06/09')
+
+--Solution (MSSQL)
+;WITH tab AS(
+SELECT id, COUNT(*) num, DENSE_RANK() OVER(Order BY COUNT(*) DESC)rk FROM 
+(SELECT requester_id AS id FROM RequestAccepted
+UNION ALL
+SELECT accepter_id AS id FROM RequestAccepted)t GROUP BY id)
+SELECT  id,num
+FROM tab WHERE rk=1
+
+--Drop Table
+DROP TABLE RequestAccepted
