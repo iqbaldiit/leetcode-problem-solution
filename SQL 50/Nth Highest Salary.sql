6af36a7d05cf5d855028b26572f2831603b6aac7
iqbaldiit <iqbaldiit@gmail.com>
2025-03-20 22:53:52 +0600

    getNthHighestSalary

--- SQL 50/Nth Highest Salary.sql
+++ SQL 50/Nth Highest Salary.sql
@@ -1,3 +1,6 @@
+--Source (MSSQL): https://leetcode.com/problems/nth-highest-salary/solutions/6556302/simple-best-solution-by-iqbaldiit-9syd/
+--Source (mySQL): https://leetcode.com/problems/nth-highest-salary/solutions/6559900/simple-best-solution-by-iqbaldiit-50ek/
+--Source (pgSQL): https://leetcode.com/problems/nth-highest-salary/solutions/6560096/simple-best-solution-by-iqbaldiit-h76l/
 /*
 	Table: Employee
 
@@ -50,3 +53,58 @@
 	| null                   |
 	+------------------------+
 */
+
+--Create table
+CREATE TABLE Employee (id int,salary int);
+
+--Variable declaration
+DECLARE @N AS INT=1
+
+--Insert records
+INSERT INTO Employee VALUES (1,100);
+INSERT INTO Employee VALUES (2,100);
+INSERT INTO Employee VALUES (3,300);
+
+--Solution (MSSQL)
+SELECT DISTINCT salary AS [getNthHighestSalary(2)] 
+FROM (
+	SELECT *, DENSE_RANK() OVER (Order By Salary DESC) RK FROM Employee
+)tab WHERE tab.RK=@N
+
+-- --Solution (mySQL)
+-- N=2;
+-- SELECT DISTINCT salary AS getNthHighestSalary 
+-- FROM (
+--     SELECT salary, DENSE_RANK() OVER (ORDER BY salary DESC) AS RK 
+--     FROM Employee
+-- ) AS tab 
+-- WHERE tab.RK = N
+
+-- -- Solution (PGSQL)
+-- DO $$  
+-- DECLARE  
+--     N INT := 2;  -- Set N = 2 (for second highest salary)
+--     result INT;
+-- BEGIN  
+--     SELECT salary INTO result
+--     FROM (
+--         SELECT salary, DENSE_RANK() OVER (ORDER BY salary DESC) AS RK 
+--         FROM Employee
+--     ) AS tab 
+--     WHERE tab.RK = N
+--     LIMIT 1;     
+
+--     -- Print the result to console
+--     RAISE NOTICE 'The %th highest salary is %', N, result;
+
+-- END $$;
+
+---- leet code submission (PgSQL)
+    -- SELECT tab.salary  FROM (
+	-- 	SELECT e.salary, DENSE_RANK() OVER (ORDER BY e.salary DESC) rk FROM Employee e
+	-- ) tab  WHERE tab.rk = N  LIMIT 1  
+
+
+
+--Drop table
+DROP TABLE Employee
