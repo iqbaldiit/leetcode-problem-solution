47a166cca4d445cfa90e906599e12d1faafd4015
iqbaldiit <iqbaldiit@gmail.com>
2025-03-25 22:08:49 +0600

    Department Highest Salary

--- SQL 50/Department Highest Salary.sql
+++ SQL 50/Department Highest Salary.sql
@@ -12,27 +12,60 @@
 	| salary       | int     |
 	| departmentId | int     |
 	+--------------+---------+
-
 	id is the primary key (column with unique values) for this table.
 	departmentId is a foreign key (reference columns) of the ID from the Department table.
 	Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
- 
+	
 
 	Table: Department
+
 	+-------------+---------+
 	| Column Name | Type    |
 	+-------------+---------+
 	| id          | int     |
 	| name        | varchar |
 	+-------------+---------+
-
 	id is the primary key (column with unique values) for this table. It is guaranteed that department name is not NULL.
 	Each row of this table indicates the ID of a department and its name.
- 
+	
 
 	Write a solution to find employees who have the highest salary in each of the departments.
+
 	Return the result table in any order.
+
 	The result format is in the following example.
+
+	
+
+	Example 1:
+
+	Input: 
+	Employee table:
+	+----+-------+--------+--------------+
+	| id | name  | salary | departmentId |
+	+----+-------+--------+--------------+
+	| 1  | Joe   | 70000  | 1            |
+	| 2  | Jim   | 90000  | 1            |
+	| 3  | Henry | 80000  | 2            |
+	| 4  | Sam   | 60000  | 2            |
+	| 5  | Max   | 90000  | 1            |
+	+----+-------+--------+--------------+
+	Department table:
+	+----+-------+
+	| id | name  |
+	+----+-------+
+	| 1  | IT    |
+	| 2  | Sales |
+	+----+-------+
+	Output: 
+	+------------+----------+--------+
+	| Department | Employee | Salary |
+	+------------+----------+--------+
+	| IT         | Jim      | 90000  |
+	| Sales      | Henry    | 80000  |
+	| IT         | Max      | 90000  |
+	+------------+----------+--------+
+	Explanation: Max and Jim both have the highest salary in the IT department and Henry has the highest salary in the Sales department.
 */
 --Create table
 CREATE TABLE Employee (id int, name varchar(100),salary int,departmentId int);
@@ -48,7 +81,7 @@ INSERT INTO Employee VALUES(5,'Max',90000,1);
 INSERT INTO Department VALUES(1,'IT');
 INSERT INTO Department VALUES(2,'Sales');
 
---Solution Approach: 1 (MSSQL, MySQL)
+--Solution Approach: 1 (MSSQL, MySQL, PgSQL)
 SELECT D.name AS Department, E.name AS Employee, tab.Salary 
 FROM  (
 	SELECT E.departmentId, MAX(E.salary) Salary 
@@ -57,14 +90,14 @@ FROM  (
 INNER JOIN Employee E ON E.departmentId=tab.departmentId AND E.salary=tab.Salary
 INNER JOIN Department D ON D.id=tab.departmentId
 
---Solution Approach: 2 (MSSQL)
-;WITH tab AS (
-	SELECT E.departmentId,E.name AS Employee, E.salary, DENSE_RANK () OVER (PARTITION BY E.departmentId ORDER BY E.salary DESC) AS rk
-	FROM Employee E 
-)
-SELECT D.name AS Department, tab.Employee,tab.salary  FROM tab
-INNER JOIN Department D ON tab.departmentId=D.id
-WHERE tab.rk=1
+----Solution Approach: 2 (MSSQL)
+-- ;WITH tab AS (
+-- 	SELECT E.departmentId,E.name AS Employee, E.salary, DENSE_RANK () OVER (PARTITION BY E.departmentId ORDER BY E.salary DESC) AS rk
+-- 	FROM Employee E 
+-- )
+-- SELECT D.name AS Department, tab.Employee,tab.salary  FROM tab
+-- INNER JOIN Department D ON tab.departmentId=D.id
+-- WHERE tab.rk=1
 
 --Drop table
 DROP TABLE Employee
