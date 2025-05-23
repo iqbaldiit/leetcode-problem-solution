5d9da38b397d3893813243394668d78059beb487
iqbaldiit <iqbaldiit@gmail.com>
2025-05-23 17:55:15 +0600

    finds student

--- SQL 50/Finds Students Who Improved.sql
+++ SQL 50/Finds Students Who Improved.sql
@@ -1,5 +1,6 @@
 --source(mssql): https://leetcode.com/problems/find-students-who-improved/solutions/6761966/simple-best-solution/
 --source(mySQL): https://leetcode.com/problems/find-students-who-improved/solutions/6765239/simple-best-solution-by-iqbaldiit-b7y4/
+--source(pgsql): https://leetcode.com/problems/find-students-who-improved/solutions/6768762/simple-best-solution-by-iqbaldiit-2ev2/
 /*
 	Table: Scores
 
