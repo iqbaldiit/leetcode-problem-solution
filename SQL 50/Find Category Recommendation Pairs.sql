b56bfd057bc5769e5f13128da79028dd346757db
iqbaldiit <iqbaldiit@gmail.com>
2025-07-04 12:14:48 +0600

    Find Category Recommendation Pairs

--- SQL 50/Find Category Recommendation Pairs.sql
+++ SQL 50/Find Category Recommendation Pairs.sql
@@ -1,4 +1,5 @@
 --Source (MSSQL): https://leetcode.com/problems/find-category-recommendation-pairs/solutions/6914589/simple-best-solution-by-iqbaldiit-bbe7/
+--Source (MySQL): https://leetcode.com/problems/find-category-recommendation-pairs/solutions/6918020/simple-best-solution-by-iqbaldiit-9x2m/
 /*
 	Table: ProductPurchases
 
