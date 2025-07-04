a0769ccc9c66eeb84facd5cfb3474916af3c10a7
iqbaldiit <iqbaldiit@gmail.com>
2025-07-04 12:13:14 +0600

    Find Category Recommendation Pairs

--- SQL 50/Find Category Recommendation Pairs.sql
+++ SQL 50/Find Category Recommendation Pairs.sql
@@ -148,7 +148,7 @@ insert into ProductInfo (product_id, category, price) values ('202', 'Clothing',
 insert into ProductInfo (product_id, category, price) values ('301', 'Sports', '75')
 insert into ProductInfo (product_id, category, price) values ('401', 'Kitchen', '50');
 
---Solution(MSSQL) 
+--Solution (MSSQL) 
 
 WITH UserCategories AS (
     SELECT DISTINCT PP.user_id, P.category FROM ProductPurchases PP
