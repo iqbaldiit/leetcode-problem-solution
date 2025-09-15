8510b099f55e70455f69872cc5bddefb8e3f9b6e
iqbaldiit <iqbaldiit@gmail.com>
2025-09-15 11:20:36 +0600

    Find Zombie Session

--- SQL 50/Find Zombie Session.sql
+++ SQL 50/Find Zombie Session.sql
@@ -150,6 +150,24 @@ FROM zombie_session Z
 WHERE duration>30 AND scroll_count>4 AND (click_count*1.00/scroll_count)<0.20 AND purchase_count<=0
 ORDER BY scroll_count DESC, session_id ASC;
 
+----Solution (MySQL)
+--WITH zombie AS (
+--	SELECT A.session_id
+--	,A.user_id
+--	,MIN(event_timestamp) AS session_start
+--	,MAX(event_timestamp) AS session_end
+--	,COUNT(CASE WHEN A.event_type='scroll' THEN 1 END) AS scroll_count
+--	,COUNT(CASE WHEN A.event_type='click' THEN 1 END) AS click_count
+--	,COUNT(CASE WHEN A.event_type='purchase' THEN 1 END) AS purchase_count
+--	FROM app_events A GROUP BY A.session_id, A.user_id
+--), zombie_session AS (
+--	SELECT Z.*,TIMESTAMPDIFF(MINUTE,Z.session_start,Z.session_end) AS duration FROM zombie Z
+--)
+--SELECT Z.session_id,Z.user_id,Z.duration AS session_duration_minutes, Z.scroll_count 
+--FROM zombie_session Z
+--WHERE duration>30 AND scroll_count>4 AND (click_count*1.00/scroll_count)<0.20 AND purchase_count<=0
+--ORDER BY scroll_count DESC, session_id ASC;
+
 
 -- drop table
 DROP TABLE app_events
