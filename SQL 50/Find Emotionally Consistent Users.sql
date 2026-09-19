d91a5d242e409d7d2d0e744a6dbef219cf5f96f9
iqbaldiit <iqbaldiit@gmail.com>
2026-09-19 18:08:25 +0600

    Find Emotionally Consistent Users.sql

--- SQL 50/Find Emotionally Consistent Users.sql
+++ SQL 50/Find Emotionally Consistent Users.sql
@@ -1,78 +1,124 @@
+--Source (MSSQL,PostGresSQL,MySQL):https://leetcode.com/problems/find-emotionally-consistent-users/solutions/8529427/simple-best-solution-by-iqbaldiit-ioug/
 /*
-Table: reactions
+	Table: reactions
 
-+--------------+---------+
-| Column Name  | Type    |
-+--------------+---------+
-| user_id      | int     |
-| content_id   | int     |
-| reaction     | varchar |
-+--------------+---------+
-(user_id, content_id) is the primary key (unique value) for this table.
-Each row represents a reaction given by a user to a piece of content.
-Write a solution to identify emotionally consistent users based on the following requirements:
+	+--------------+---------+
+	| Column Name  | Type    |
+	+--------------+---------+
+	| user_id      | int     |
+	| content_id   | int     |
+	| reaction     | varchar |
+	+--------------+---------+
+	(user_id, content_id) is the primary key (unique value) for this table.
+	Each row represents a reaction given by a user to a piece of content.
+	Write a solution to identify emotionally consistent users based on the following requirements:
 
-For each user, count the total number of reactions they have given.
-Only include users who have reacted to at least 5 different content items.
-A user is considered emotionally consistent if at least 60% of their reactions are of the same type.
-Return the result table ordered by reaction_ratio in descending order and then by user_id in ascending order.
+	For each user, count the total number of reactions they have given.
+	Only include users who have reacted to at least 5 different content items.
+	A user is considered emotionally consistent if at least 60% of their reactions are of the same type.
+	Return the result table ordered by reaction_ratio in descending order and then by user_id in ascending order.
 
-Note:
+	Note:
 
-reaction_ratio should be rounded to 2 decimal places
-The result format is in the following example.
+	reaction_ratio should be rounded to 2 decimal places
+	The result format is in the following example.
 
  
 
-Example:
+	Example:
 
-Input:
+	Input:
 
-reactions table:
+	reactions table:
 
-+---------+------------+----------+
-| user_id | content_id | reaction |
-+---------+------------+----------+
-| 1       | 101        | like     |
-| 1       | 102        | like     |
-| 1       | 103        | like     |
-| 1       | 104        | wow      |
-| 1       | 105        | like     |
-| 2       | 201        | like     |
-| 2       | 202        | wow      |
-| 2       | 203        | sad      |
-| 2       | 204        | like     |
-| 2       | 205        | wow      |
-| 3       | 301        | love     |
-| 3       | 302        | love     |
-| 3       | 303        | love     |
-| 3       | 304        | love     |
-| 3       | 305        | love     |
-+---------+------------+----------+
-Output:
+	+---------+------------+----------+
+	| user_id | content_id | reaction |
+	+---------+------------+----------+
+	| 1       | 101        | like     |
+	| 1       | 102        | like     |
+	| 1       | 103        | like     |
+	| 1       | 104        | wow      |
+	| 1       | 105        | like     |
+	| 2       | 201        | like     |
+	| 2       | 202        | wow      |
+	| 2       | 203        | sad      |
+	| 2       | 204        | like     |
+	| 2       | 205        | wow      |
+	| 3       | 301        | love     |
+	| 3       | 302        | love     |
+	| 3       | 303        | love     |
+	| 3       | 304        | love     |
+	| 3       | 305        | love     |
+	+---------+------------+----------+
+	Output:
 
-+---------+-------------------+----------------+
-| user_id | dominant_reaction | reaction_ratio |
-+---------+-------------------+----------------+
-| 3       | love              | 1.00           |
-| 1       | like              | 0.80           |
-+---------+-------------------+----------------+
-Explanation:
+	+---------+-------------------+----------------+
+	| user_id | dominant_reaction | reaction_ratio |
+	+---------+-------------------+----------------+
+	| 3       | love              | 1.00           |
+	| 1       | like              | 0.80           |
+	+---------+-------------------+----------------+
+	Explanation:
 
-User 1:
-Total reactions = 5
-like appears 4 times
-reaction_ratio = 4 / 5 = 0.80
-Meets the 60% consistency requirement
-User 2:
-Total reactions = 5
-Most frequent reaction appears only 2 times
-reaction_ratio = 2 / 5 = 0.40
-Does not meet the consistency requirement
-User 3:
-Total reactions = 5
-'love' appears 5 times
-reaction_ratio = 5 / 5 = 1.00
-Meets the consistency requirement
-The Results table is ordered by reaction_ratio in descending order, then by user_id in ascending order.
+	User 1:
+	Total reactions = 5
+	like appears 4 times
+	reaction_ratio = 4 / 5 = 0.80
+	Meets the 60% consistency requirement
+	User 2:
+	Total reactions = 5
+	Most frequent reaction appears only 2 times
+	reaction_ratio = 2 / 5 = 0.40
+	Does not meet the consistency requirement
+	User 3:
+	Total reactions = 5
+	'love' appears 5 times
+	reaction_ratio = 5 / 5 = 1.00
+	Meets the consistency requirement
+	The Results table is ordered by reaction_ratio in descending order, then by user_id in ascending order.
 */
+
+
+CREATE TABLE reactions (
+    user_id INT,
+    content_id INT,
+    reaction VARCHAR(255),
+    PRIMARY KEY (user_id, content_id)
+);
+
+INSERT INTO reactions (user_id, content_id, reaction) VALUES
+(1, 101, 'like'),
+(1, 102, 'like'),
+(1, 103, 'like'),
+(1, 104, 'wow'),
+(1, 105, 'like'),
+(2, 201, 'like'),
+(2, 202, 'wow'),
+(2, 203, 'sad'),
+(2, 204, 'like'),
+(2, 205, 'wow'),
+(3, 301, 'love'),
+(3, 302, 'love'),
+(3, 303, 'love'),
+(3, 304, 'love'),
+(3, 305, 'love');
+
+--Solutions
+SELECT * FROM reactions
+
+;WITH tbl_total_reaction AS(
+	SELECT user_id,COUNT(1) AS total_reactions FROM reactions GROUP BY user_id HAVING COUNT(1)>=5
+), tbl_dom_reaction AS (
+	SELECT user_id,reaction,COUNT(1) AS reaction_count FROM reactions GROUP BY user_id ,reaction
+), tbl_result AS (
+	SELECT tr.user_id,dr.reaction AS dominant_reaction
+	,ROUND(1.00*dr.reaction_count/tr.total_reactions,2) AS reaction_ratio
+	FROM tbl_total_reaction tr
+	INNER JOIN tbl_dom_reaction dr ON tr.user_id=dr.user_id
+	WHERE 1.00*dr.reaction_count/tr.total_reactions>=0.6
+)
+SELECT * FROM tbl_result ORDER BY reaction_ratio DESC, user_id ASC
+
+
+--Drop Table
+DROP TABLE reactions
